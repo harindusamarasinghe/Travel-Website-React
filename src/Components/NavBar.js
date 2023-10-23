eec8ba99a2dfc4ea0755236489d6db867a578126
Shaun <shaun@Shauns-MacBook-Air.local>
2023-10-23 12:21:17 +0530

    Made Changes to the NavBar and Added Some Styles

--- src/Components/NavBar.js
+++ src/Components/NavBar.js
@@ -1,48 +1,47 @@
-import React ,{useState} from 'react';
+import React ,{Component} from 'react';
 import './NavBarStyles.css';
-import Home from '../Icons/home-smile-svgrepo-com.svg';
-import Tour from '../Icons/travel-expenses-svgrepo-com.svg';
-import Gallery from '../Icons/gallery-svgrepo-com.svg';
-import About from '../Icons/about-svgrepo-com (1).svg';
-import Contact from '../Icons/contact-svgrepo-com.svg';
 
 
-export default function NavBar(){
-    const[menuOpen,setMenuOpen]=useState(false);
+
+export default class NavBar extends Component{
+    state= {clicked:false};
+    handlelick=()=>{
+        this.setState({clicked:!this.state.clicked})
+    }
+    render(){
     return(
         <div>
             <nav className='navbar--items' >
                 <h1>CocoShores</h1>
-                <div className='menu' onClick={()=>{
-                        setMenuOpen(!menuOpen);
-                    }}>
-                    <span></span>
-                    <span></span>
-                    <span></span>
-                </div>
-                <ul className={menuOpen?"open":""}>
+                <ul id='navbar' className={this.state.clicked?"#navbar active":"#navbar"}>
                     <li className='items'>
-                        <img src={Home} alt='Home Icon'/>
+                        
                         <a href='/' className='links'>Home</a>
                     </li>
                     <li className='items'>
-                        <img src={Tour} alt='Travel Icon'/>
+                         
                         <a href='/' className='links'>Tour Packages</a>
                     </li>
                     <li className='items'>
-                        <img src={Gallery} alt='Gallery Icon'/>
+                        
                         <a href='/' className='links' >Gallery</a>
                     </li>
                     <li className='items'>
-                        <img src={About} alt='About Icon'/>
+                        
                         <a href='/' className='links' >About</a>
                     </li>
                     <li className='items'>
-                        <img src={Contact} alt='Home Icon'/>
+                        
                         <a href='/' className='links' >Contact</a>
                     </li>
                 </ul>
+                <div id='mobile' onClick={this.handlelick}>
+                    <i
+                    id="bar"
+                    className={this.state.clicked? "fa fa-close":"fas fa-bars"}>    
+                    </i>
+                </div>
             </nav>
         </div>
     )
-}
+}}
